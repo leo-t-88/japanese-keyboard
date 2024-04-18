2169c9b8b85b1be6d84de367002ff03a8e428ce3
Léo.t88 <84191908+leo-t-88@users.noreply.github.com>
2024-04-18 18:53:41 +0200

    Update 1.1.1

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.IO;
 
 namespace Clavier_Jap;
 /// <summary>
@@ -8,4 +9,21 @@ namespace Clavier_Jap;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        if (!ExistImg("assets/azerty.png") || !ExistImg("assets/qwerty.png") || !ExistImg("assets/smartinput.png") || !ExistImg("assets/default.jpg") || !ExistImg("assets/custom.jpg") || !ExistImg("assets/changebg.jpg"))
+        {
+            MessageBoxResult result = MessageBox.Show("Some images needed for the application to work are missing.\n\nPlease reinstall the application or the missing images.", "Error - Missing images", MessageBoxButton.OK, MessageBoxImage.Error);
+            if (result == MessageBoxResult.OK)
+            {
+                Shutdown();
+            }
+        }
+    }
+    private bool ExistImg(string cheminImage)
+    {
+        return File.Exists(cheminImage);
+    }
 }
